3a884cabfb5aa678abb5e5048eaafafed92fbb1d
ayushyachitransh <ayushyachitransh@gmail.com>
2017-06-02 00:22:20 +0530

    Added 'release' as submodule
    Release folder added as submodule
    > setup.sh: check if release already exists, then update it, else add it as submodule

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+# setup environment variable for getting tomcat password
 if [[ -z "${TOMCAT_DOCKER_ENV_PASSWORD}" ]]; then
 	echo "Set a password for docker:"
 	read -s TOMCAT_DOCKER_ENV_PASSWORD
@@ -14,3 +15,23 @@ else
 		echo "Incorrect password"
 	fi
 fi
+
+# add release as submodule to get various contents.
+repo="release"
+if [ ! -d $repo ]
+then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  # Add submodule Release
+  echo "Creating :" $repo
+  git submodule add https://github.com/reactome/$repo\
+else
+  # release already exists, so just update repository and discard local changes
+  cd $repo
+  echo "Discarding local changes to release"
+  git fetch --all
+  git reset --hard origin/master
+  echo "Updating " $repo
+  git pull origin master
+  cd ..
+fi
+
