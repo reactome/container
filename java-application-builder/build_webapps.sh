bcc7e2082c303ead893f01f70e5e248e79492075
ayushyachitransh <ayushyachitransh@gmail.com>
2017-07-01 12:39:54 +0530

    Build  Reactome.jar
    Modified ReactomeJar.xml: Earlier only org was included in jar, but included all files within src for now. It can be modified later as per needs.
    
    Modified build_webapps.sh: Mount junit-4.12.jar which was downloaded from https://mvnrepository.com/artifact/junit/junit/4.12, it shall not be needed when CuratorTool is updated.
    
    Added junit-4.12.jar: Jar file required to compile some java files inside CuratorTool/src/org

--- java-application-builder/build_webapps.sh
+++ java-application-builder/build_webapps.sh
@@ -6,6 +6,7 @@ set -x
 docker run -it --name=java-webapp-builder --rm -v "$(pwd)/webapps:/webapps" \
 	-v "$(pwd)/Pathway-Exchange-pom.xml:/gitroot/Pathway-Exchange/pom.xml" \
 	-v "$(pwd)/ReactomeJar.xml:/gitroot/CuratorTool/ant/ReactomeJar.xml" \
+	-v "$(pwd)/junit-4.12.jar:/gitroot/CuratorTool/lib/junit/junit-4.12.jar" \
 	-v "$(pwd)/RESTfulAPI-pom.xml:/gitroot/RESTfulAPI/pom.xml" \
 	-v "$(pwd)/m2-cache:/root/.m2" \
 	reactome-app-builder \
