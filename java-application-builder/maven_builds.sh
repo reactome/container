e087c489c194f6972ab969902a96682a8b085440
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-07-04 11:35:11 -0400

    Integrate Ayushya's changes with mine.
    Ayushya has gotten reactome.jar to build successfully. I've integrated his changes with some of the changes I had.
    
    My changes included some changes for ReactomeRESTfulAPI to include some dependencies (spring and now slf4j as Ayushya pointed out that there were some version incompatibilities with slf4j).
    
    Also added some .gitignore files.
    
    Also updated Pathway-Exchange-pom.xml to use the new reactome.jar.
    
    Also updated maven_builds.sh to use Ayushya's code to create reactome.jar and still build Pathway-Exchange.

--- java-application-builder/maven_builds.sh
+++ java-application-builder/maven_builds.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
+
+cd /gitroot/libsbgn && ant && \
 cd /gitroot/CuratorTool/ant && \
 ant -buildfile ReactomeJar.xml && \
-ant -buildfile CuratorToolBuild.xml &&\
-# cd /gitroot/libsbgn && ant && \
-# cd /gitroot/Pathway-Exchange \
-# && mvn install:install-file -Dfile=/gitroot/libsbgn/dist/org.sbgn.jar -DartifactId=sbgn -DgroupId=org.sbgn -Dpackaging=jar -Dversion=milestone2 \
-# && mvn install:install-file -Dfile=./lib/celldesigner/celldesigner.jar -DgroupId=celldesigner -DartifactId=celldesigner -Dversion=UNKNOWN_VERSION -Dpackaging=jar \
-# && mvn install:install-file -Dfile=./lib/protege/arq.jar -DgroupId=com.hp.hpl.jena -DartifactId=arq -Dpackaging=jar -Dversion=UNKNOWN_VERSION \
-# && mvn install:install-file -Dfile=./lib/protege/protege.jar -Dpackaging=jar -DgroupId=edu.stanford.protege -DartifactId=protege -Dversion=UNKNOWN_VERSION \
-# && mvn install:install-file -Dfile=./lib/protege/protege-owl.jar -Dversion=3.2 -DgroupId=edu.stanford.smi.protege -DartifactId=protege-owl -Dpackaging=jar \
-# && mvn install:install-file -Dfile=lib/sbml/jsbml-0.8-rc1-with-dependencies.jar -DgroupId=org.sbml -DartifactId=jsbml -Dversion=0.8-rc1 -Dpackaging=jar \
-# && mvn install:install-file -Dfile=./lib/sbml/libsbmlj.jar -DgroupId=org.sbml -DartifactId=libsbml -Dpackaging=jar -Dversion=0.8-rc1 \
-# && pwd && mvn compile package install && \
+ant -buildfile CuratorToolBuild.xml && \
 cd /gitroot/RESTfulAPI/ \
 && mvn install:install-file -Dfile=/gitroot/CuratorTool/reactome.jar -DartifactId=Reactome -DgroupId=org.reactome -Dpackaging=jar -Dversion=UNKNOWN_VERSION \
-&& ls /gitroot/RESTfulAPI/ -lht \
+&& cd /gitroot/Pathway-Exchange \
+&& mvn install:install-file -Dfile=/gitroot/libsbgn/dist/org.sbgn.jar -DartifactId=sbgn -DgroupId=org.sbgn -Dpackaging=jar -Dversion=milestone2 \
+&& mvn install:install-file -Dfile=./lib/celldesigner/celldesigner.jar -DgroupId=celldesigner -DartifactId=celldesigner -Dversion=UNKNOWN_VERSION -Dpackaging=jar \
+&& mvn install:install-file -Dfile=./lib/protege/arq.jar -DgroupId=com.hp.hpl.jena -DartifactId=arq -Dpackaging=jar -Dversion=UNKNOWN_VERSION \
+&& mvn install:install-file -Dfile=./lib/protege/protege.jar -Dpackaging=jar -DgroupId=edu.stanford.protege -DartifactId=protege -Dversion=UNKNOWN_VERSION \
+&& mvn install:install-file -Dfile=./lib/protege/protege-owl.jar -Dversion=3.2 -DgroupId=edu.stanford.smi.protege -DartifactId=protege-owl -Dpackaging=jar \
+&& mvn install:install-file -Dfile=lib/sbml/jsbml-0.8-rc1-with-dependencies.jar -DgroupId=org.sbml -DartifactId=jsbml -Dversion=0.8-rc1 -Dpackaging=jar \
+&& mvn install:install-file -Dfile=./lib/sbml/libsbmlj.jar -DgroupId=org.sbml -DartifactId=libsbml -Dpackaging=jar -Dversion=0.8-rc1 \
+&& pwd && mvn compile package install \
+&& cd /gitroot/RESTfulAPI/ -lht \
 && pwd && mvn package \
 # && cd /gitroot/browser && mvn package && \
 # cd /gitroot/diagram-exporter && mvn install && \
