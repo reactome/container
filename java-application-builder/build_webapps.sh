231a64feda7a70b8abe5f27617050f22483b1bf3
ayushyachitransh <ayushyachitransh@gmail.com>
2017-07-12 20:33:50 +0530

    Build Analysis.bin is 'Probably' working

--- java-application-builder/build_webapps.sh
+++ java-application-builder/build_webapps.sh
@@ -5,6 +5,7 @@ docker build -t reactome-app-builder  -f buildApps.dockerfile .
 echo "Running Analysis Service requires a working database"
 # we are using the tomcat database: gk_current
 # using defaults from mysql-for-tomcat container
+docker network create -d bridge --subnet 172.25.0.0/16 isolated_nw
 docker run -itd --rm \
 	--network=isolated_nw \
 	--ip=172.25.3.3 \
@@ -12,15 +13,21 @@ docker run -itd --rm \
 	--volume "$(dirname `pwd`)/mysql/tomcat_data/:/docker-entrypoint-initdb.d" \
 	--env-file $(dirname `pwd`)/tomcat.env mysql
 
-docker network connect isolated_nw mysql-for-webapps
+
+seconds=360; date1=$((`date +%s` + $seconds)); 
+while [ "$date1" -ge `date +%s` ]; do 
+  echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r"; 
+done
 
 set -x
 # Build the java applications
 docker run -it --name=java-webapp-builder --rm -v "$(pwd)/webapps:/webapps" \
+    --network=isolated_nw \
 	-v "$(pwd)/mounts/Pathway-Exchange-pom.xml:/gitroot/Pathway-Exchange/pom.xml" \
 	-v "$(pwd)/mounts/AnalysisTools-Core-pom.xml:/gitroot/AnalysisTools/Core/pom.xml" \
 	-v "$(pwd)/mounts/AnalysisTools-Service-pom.xml:/gitroot/AnalysisTools/Service/pom.xml" \
 	-v "$(pwd)/mounts/AnalysisTools-Service-web.xml:/gitroot/AnalysisTools/Service/src/main/webapp/WEB-INF/web.xml" \
+	-v "$(pwd)/mounts/interactors.db:/gitroot/AnalysisTools/interactors.db" \
 	-v "$(pwd)/mounts/ReactomeJar.xml:/gitroot/CuratorTool/ant/ReactomeJar.xml" \
 	-v "$(pwd)/mounts/JavaBuildPackaging.xml:/gitroot/CuratorTool/ant/JavaBuildPackaging.xml" \
 	-v "$(pwd)/mounts/CuratorToolBuild.xml:/gitroot/CuratorTool/ant/CuratorToolBuild.xml" \
