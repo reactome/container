8a4a19bd610d027806e84425fd245e613033987f
ayushyachitransh <ayushyachitransh@gmail.com>
2017-07-20 23:34:01 +0530

    Added skeleton for download Script
    One database file has been added currently. Other files will be added in similar manner.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
+echo -e "\n\n"
+echo "==========================================================================="
+echo "                        Collecting databases"
+echo "==========================================================================="
+echo "Details of databases to be collected are:"
+echo "In mysql-------------------------------------------------------------------"
+echo "->mysql/tomcat_data/gk_current.sql.gz"
+echo "->mysql/wordpress_data/reactome-wordpress.sql.gz"
+echo
+echo "In neo4j-------------------------------------------------------------------"
+echo "->neo4j/data/reactome.graphdb.tgz"
+# cd ./mysql/
+URL_tomcat_db=http://www.reactome.org/download/current/databases/gk_current.sql.gz
+URL_wordpress_db=http://www.reactome.org/download/current/databases/gk_wordpress.sql.gz
+URL_neo4j_db=http://reactome.org/download/current/reactome.graphdb.tgz
+
+echo "Initialting downloads..."
+# wget --timestamping --directory-prefix=mysql/tomcat_data $URL_tomcat_db
+# wget --timestamping --directory-prefix=mysql/wordpress_data $URL_wordpress_db
+# wget --timestamping --directory-prefix=neo4j/data $URL_neo4j_db
+
+remote_file_size=$(curl -sI $URL_tomcat_db | grep -i content-length | awk '{print $2}')
+local_file_size=$(ls -l ./mysql/tomcat_data/gk_current.sql.gz | awk '{print $5}')
+echo $remote_file_size
+echo $local_file_size
+echo "----------------------------------------------------------------------------"
+if [[ "$local_file_size"=="$remote_file_size" ]]; then
+    echo "Database up to date. Update not required"
+else
+    echo "Database needs to be updated!"
+    wget --continue --directory-prefix=mysql/tomcat_data $URL_tomcat_db
+fi
+echo "-----------------------Script Under development-----------------------------"
+exit
+
 read -p "Build webapps? Press y/n" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
