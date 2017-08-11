07a4978a0b4dd0ca90ba126c58ed22a5cd9b9b72
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-08-11 17:44:38 -0400

    Move the execution of the indexer to a script.
    Running the Indexer script probably should not have been a
    docker-compose entrypoint. Moved it to a separate script. Just make sure
    the composed containers and the indexer are all a part of the same
    network.

--- java-application-builder/build_solr_index.sh
+++ java-application-builder/build_solr_index.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
 
-# Meant to be run inside a container with access to a neo4j container and a solr container
+# Run the indexer in a container. Solr and neo4j must already be up and running and a
+# part of the network "javaapplicationbuilder_solr-index-builder"
 
 echo "Running indexer..."
-java -jar /Indexer-jar-with-dependencies.jar \
-	-a neo4j -b 7474 -c neo4j -d neo4j-password \
-	-e  http://solr:8983/solr/reactome -f "" -g "" \
+
+docker run  -v $(pwd)/webapps/interactors.db:/interactors.db \
+	-v $(pwd)/wait-for.sh:/wait-for.sh \
+	-v $(pwd)/webapps/Indexer-jar-with-dependencies.jar:/Indexer-jar-with-dependencies.jar \
+	-v $(pwd)/logs:/gitroot/search-indexer/logs \
+	--network=javaapplicationbuilder_solr-index-builder \
+	reactome-app-builder \
+	bash -c "/wait-for.sh neo4j:7687 -t 360 && /wait-for.sh solr:8983 -t 360 && java -jar /Indexer-jar-with-dependencies.jar \
+	-a neo4j -b 7474 -c neo4j -d neo4j-password -e  http://solr:8983/solr/reactome -f \"\" -g \"\" \
 	-h /interactors.db \
-	-i localhost -j 25 -k dummy
+	-i localhost -j 25 -k dummy "
+
+
+# java -jar /Indexer-jar-with-dependencies.jar \
+# 	-a neo4j -b 7474 -c neo4j -d neo4j-password \
+# 	-e  http://solr:8983/solr/reactome -f "" -g "" \
+# 	-h /interactors.db \
+# 	-i localhost -j 25 -k dummy
