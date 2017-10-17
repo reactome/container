98a24fe00d849ee4f7d1a05d9fea07a8237b996c
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2017-10-17 16:04:46 -0400

    consistent graphdb name
    Ensure that the graphdb is always in a directory with the same name,
    regardless of what it is in the tarball.

--- neo4j/neo4j-init.sh
+++ neo4j/neo4j-init.sh
@@ -7,7 +7,14 @@ echo "Initialization script started"
 if [ ! -f /tmp/neo4j-import-done.flag ]; then
     currentDir="$(pwd)"
     cd /var/lib/neo4j/data/databases/
-    tar -xvzf /var/lib/neo4j/data/databases/reactome.graphdb.tgz
+    # new directory - contents of reactome.graphdb.tgz will be put in here
+    # because the directory within reactome.graphdb.tgz is inconsistently named
+    # (for example: reactome.graphdb.61 vs. reactome.graphdb.v62 - notice the
+    # extra "v") and neo4j needs an EXACT directory name, so we will ensure
+    # that the data will *always* be in "reactome.graphdb" - then we never
+    # need to update dbms.active_database in neo4j.conf
+    mkdir reactome.graphdb
+    tar -C reactome.graphdb --strip-components=1  -xvzf reactome.graphdb.tgz
     cd $currentDir
     touch /tmp/neo4j-import-done.flag
 else
