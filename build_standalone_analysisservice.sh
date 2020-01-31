01659d8ac8a53c4daef9d40fff04c770b0e676f9
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2020-01-31 15:16:09 -0500

    Script for stand-alone analysis-service

--- build_standalone_analysisservice.sh
+++ build_standalone_analysisservice.sh
@@ -15,29 +15,29 @@ docker build -t reactome/grapdb:$RELEASE_VERSION \
 	--build-arg GRAPHDB_LOCATION=https://reactome.org/download/current/reactome.graphdb.tgz \
 	-f ./neo4j_stand-alone.dockerfile .
 
-echo -e "===\nBuilding solr index...\n"
-cd $STARTING_DIR/solr
-docker build -t reactome/solr:$RELEASE_VERSION \
+echo -e "===\nCreating the Analyis file...\n"
+cd $STARTING_DIR/analysis-core
+docker build -t reactome/analysis-core \
+	--build-arg RELEASE_VERSION=$RELEASE_VERSION \
 	--build-arg NEO4J_USER=$NEO4J_USER \
 	--build-arg NEO4J_PASSWORD=$NEO4J_PASSWORD \
-	--build-arg RELEASE_VERSION=$RELEASE_VERSION \
-	-f index-builder.dockerfile .
+	-f analysis-core.dockerfile .
 
-echo -e "===\nGenerating diagram files...\n"
-cd $STARTING_DIR/diagram-generator
+echo -e "===\nGenerating Fireworks files...\n"
+cd $STARTING_DIR/fireworks-generator
+docker build -t reactome/fireworks-generator \
+	--build-arg RELEASE_VERSION=$RELEASE_VERSION \
+	--build-arg NEO4J_USER=$NEO4J_USER \
+	--build-arg NEO4J_PASSWORD=$NEO4J_PASSWORD \
+	-f fireworks-generator.dockerfile .
+
+echo -e "===\nBuilding analysis-service image...\n"
+cd $STARTING_DIR/stand-alone-analysis-service
 docker build -t reactome/diagram-generator \
 	--build-arg NEO4J_USER=$NEO4J_USER \
 	--build-arg NEO4J_PASSWORD=$NEO4J_PASSWORD \
 	--build-arg RELEASE_VERSION=$RELEASE_VERSION \
-	-f diagram-generator.dockerfile .
-
-echo -e "===\nBuilding content-service image...\n"
-cd $STARTING_DIR/stand-alone-content-service
-docker build -t reactome/diagram-generator \
-	--build-arg NEO4J_USER=$NEO4J_USER \
-	--build-arg NEO4J_PASSWORD=$NEO4J_PASSWORD \
-	--build-arg RELEASE_VERSION=$RELEASE_VERSION \
-	-f content-service.dockerfile .
+	-f analysis-service.dockerfile .
 
 cd $STARTING_DIR/
 
@@ -45,5 +45,5 @@ echo -e "===\nImages built: "
 # We are building 4 "reactome" images, so lets display them
 docker images | grep "reactome" | head -n 4
 
-echo -e "Now you can run the stand-alone content-service like this:\n'docker run --name reactome-content-service -p 8080:8080 reactome/stand-alone-content-service:Release71'"
+echo -e "Now you can run the stand-alone content-service like this:\n'docker run --name reactome-analysis-service -p 8080:8080 reactome/stand-alone-analysis-service:${RELEASE_VERSION}'"
 set +e
