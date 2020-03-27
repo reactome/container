afddaf317f0e3cf2298eb5cc52bd02f1b465b0be
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2020-03-27 13:42:45 -0400

    Fixed command to run analysis-core

--- analysis-core/entrypoint.sh
+++ analysis-core/entrypoint.sh
@@ -11,4 +11,4 @@ bash /docker-entrypoint.sh neo4j &
 echo "Waiting for Neo4j..."
 bash /wait-for.sh localhost:7687 -t 90 && bash /wait-for.sh localhost:7474 -t 90
 
-time java -jar /applications/analysis-core-jar-with-dependencies.jar -h localhost -p 7474 -u $NEO4J_USER -k $NEO4J_PASSWORD -o /output/analysis.bin -t -v
+time java -jar /applications/analysis-core-jar-with-dependencies.jar -h localhost -p 7474 -u $NEO4J_USER -k $NEO4J_PASSWORD -o /output/analysis.bin -v
